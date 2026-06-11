1ef10fc55c9f0c7795aa8d29070147545ad3b688
Minhaz <minhazuddinsapui@gmail.com>
2026-06-11 19:58:04 +0530

    Fix CORS__

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,28 +1,29 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-    origin:  [
-        "http://localhost:5173",
-        "https://interview-ai-nine-pi.vercel.app"
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://interview-ai-nine-pi.vercel.app",
+      "https://interview-n6b4ru5hd-minhazuddin-sapuis-projects.vercel.app",
+      "https://interview-ai-git-main-minhazuddin-sapuis-projects.vercel.app",
     ],
-    credentials: true
-}))
+    credentials: true,
+  }),
+);
 
 /* require all the routes here */
-const authRouter = require("./routes/auth.routes")
-const interviewRouter = require("./routes/interview.routes")
-
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
 
 /* using all the routes here */
-app.use("/api/auth", authRouter)
-app.use("/api/interview", interviewRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
 
-
-
-module.exports = app
+module.exports = app;
